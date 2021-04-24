cecf264bb481aecb449762be5d31d62730780da4
Alex <aza5975@psu.edu>
2021-04-24 16:05:49 -0400

    Small Screen adjustment, light button alpha

---
+++ lightDarkMode.js
@@ -0,0 +1,8 @@
+function tougleThemeColor() {
+    var body = document.body;
+    body.classList.toggle("light-mode");
+
+    var buttons = document.getElementById("buttons");
+    buttons.classList.tougle("light-mode");
+ }
+
