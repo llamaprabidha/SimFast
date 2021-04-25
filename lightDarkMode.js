c7496b7cf818860c67693daa333e3e981043fafa
Alex <aza5975@psu.edu>
2021-04-25 15:38:00 -0400

    more size adj

--- lightDarkMode.js
+++ lightDarkMode.js
@@ -1,8 +1,4 @@
 function tougleThemeColor() {
-    var body = document.body;
+    const body = document.body;
     body.classList.toggle("light-mode");
-
-    var buttons = document.getElementById("buttons");
-    buttons.classList.tougle("light-mode");
- }
-
+}
